1eea484a3c3cce9f2a77b71f4d80f9d1c8b99c10
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2019-12-29 15:10:38 -0500

    adding button that scrolls directly to projects section

--- src/hooks/projects.js
+++ src/hooks/projects.js
@@ -9,17 +9,14 @@ function Projects() {
 
       {
         websites.map(website => {
-         const projectStyle = {
-           backgroundColor: 'blue'
-         
-         };
+      
 
           return (
             <div class='flip-card-container'>
             <div class='flip-card'> 
 <div class='flip-card-front'>
             <img class='p-img' src={require(`${website.photos}`)}/>
-            <div class='card-footer' style={projectStyle}> 
+            <div class='card-footer' > 
             <h1 class='p-name'>{website.name} </h1>
             <li class='p-features'>{website.keyFeatures}  </li> 
             </div>
