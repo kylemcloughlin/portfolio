e5ddc884081b8d687369df4fc830f80aad8f7b4b
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2020-02-11 15:02:01 -0500

    added more styling

--- src/hooks/modal.js
+++ src/hooks/modal.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 
 const customStyles = {
@@ -9,26 +8,22 @@ const customStyles = {
     right                 : 'auto',
     bottom                : 'auto',
     marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+    transform             : 'translate(-50%, -50%)',
+    width                 : '88%',
+    height                : '79%',
+    background            : '#0B0C10'
   }
 };
 
 // Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
 
 function DemoModal({ gif }){
-console.log(gif)
-console.log(gif)
   var subtitle;
   const [modalIsOpen,setIsOpen] = useState(false);
   function openModal() {
     setIsOpen(true);
   }
-  
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
-  
+ 
   function closeModal(){
     setIsOpen(false);
   }
@@ -41,28 +36,18 @@ console.log(gif)
         <button class='demo-but' onClick={openModal}>Demo</button>
         <Modal
           isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
           onRequestClose={closeModal}
           style={customStyles}
-          contentLabel="Example Modal"
+          contentLabel="Demo-Modal"
+          // id='modal'
         >
-
-          <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
-          <img src={require(`${gif}`)}/>
-          <button onClick={closeModal}>close</button>
-          <div>I am a modal</div>
-          {/* <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form> */}
+          <img class='demo-gif' src={require(`${gif}`)}/>
+          <button onClick={closeModal}>close</button>  
         </Modal>
       </div>
     );
 }
 
-// ReactDOM.render(<DemoModal />, appElement); 
+
 
 export default DemoModal;
